469c0bd258faa931afaada622958d159c081fd7c
berkenpas <berkenpasw@gmail.com>
2022-07-18 21:58:14 -0500

    feat: base routing from app.jsx, skeleton of home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import './App.css';
-import React from 'react';
+import React, {
+  useEffect, createContext, useReducer, useContext,
+} from 'react';
+import {
+  BrowserRouter, Route, Routes, useNavigate,
+} from 'react-router-dom';
+import Home from './components/pages/home';
+import { reducer, initialState } from './reducers/userReducer';
+
+export const UserContext = createContext();
+
+const Routing = () => {
+  const navigate = useNavigate();
+  /* eslint-disable */
+  const { state, dispatch } = useContext(UserContext);
+  /* eslint-enable */
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      dispatch({ type: 'USER', payload: user });
+    } else {
+      navigate('/');
+    }
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/* eslint-disable */
+const HomeOfficial = () => {
+/* eslint-enable */
+  return (
+    <div>
+      Home page for Programming Party
+    </div>
+  );
+};
+
+export default HomeOfficial;
