4f69a6c5185fe48d7456e545a6ab755f10169e49
Chris <chrishollandaise@gmail.com>
2022-07-20 23:21:24 -0700

    chore: added some more to the eslint ignore list

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,12 @@ module.exports = {
     'react',
   ],
   rules: {
-	  "linebreak-style": 0,
-	  "global-require": 0,
-	  "react/function-component-definition" : 0,
-	  "react/jsx-no-constructed-context-values": 0,
-	  "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    'linebreak-style': 0,
+    'global-require': 0,
+    'react/function-component-definition': 0,
+    'react/jsx-no-constructed-context-values': 0,
+    'react/prop-types': 0,
+    'react/destructuring-assignment': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
 };
